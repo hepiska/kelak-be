5c4e774d2f69d53e067b5c0a5e789054156adcf4
hepiska <ego1403@gmail.com>
2020-08-06 00:41:25 +0700

    global cart init

--- src/controlers/article.js
+++ src/controlers/article.js
@@ -2,7 +2,7 @@ import articleDa from "dataAccess/article"
 import joi from "@hapi/joi"
 import { parseSort, stringToQueryObj } from "libs/helpers"
 import { uploadImage } from "libs/images"
-import { articleTypes } from "../../utis/constants"
+import { articleTypes } from "../utis/constants"
 
 
 const ArticleSchema = joi.object().keys({
