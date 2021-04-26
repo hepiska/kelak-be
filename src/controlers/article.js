970d1ac5261e50b030f6a7a00a73cc7693fbdbaa
ego <ego1403@gmail.com>
2021-04-26 10:43:16 +0700

    refamp

--- src/controlers/article.js
+++ src/controlers/article.js
@@ -111,7 +111,7 @@ const articleControlers = {
 
 
       if (category) {
-        const cat = await categoryModel.find({ slug: query })
+        const cat = await categoryModel.find({ slug: category })
 
         query.categories = cat._id
       }
